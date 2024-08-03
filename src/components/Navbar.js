2a655ca93f4aa6437ee685e98b290130b1dda9ff
Manoj <145483133+manoj0277@users.noreply.github.com>
2024-08-03 21:44:22 +0530

    remove about page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,12 +11,12 @@ export default function Navbar(props) {
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
+                        {/* <li className="nav-item">
                             <Link className="nav-link active" aria-current="page" to="/">{props.Home}</Link>
-                        </li>
-                        <li className="nav-item">
+                        </li> */}
+                        {/* <li className="nav-item">
                             <Link className="nav-link active" aria-current="page"  to="/about">{props.About}</Link>
-                        </li>
+                        </li> */}
                     </ul>
                     {/* <form className="d-flex">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
